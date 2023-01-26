8cc732475435bf86a35f6a79d98272ed4100e541
Mario Junior <mario-cruzeiro@hotmail.com>
2023-01-26 10:01:59 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,70 @@
 import React from 'react';
-import Logo from './img/logo.png';
+import { Link } from 'react-router-dom';
 import {MdShoppingCart} from 'react-icons/md';
+import { motion } from 'framer-motion';
+import { useStateValue } from '../context/StateProvider';
+import {actionType} from '../context/reducer';
+
+import { getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import {app} from '../firebase.config';
+
+import Logo from './img/logo.png';
+import Avatar from './img/avatar.png';
+
 
 
 const Header = () => {
+
+    const firebaseAuth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    const [{user}, dispatch] = useStateValue()
+
+  const login = async() => {
+    const response = await signInWithPopup(firebaseAuth, provider);
+    console.log(response);
+/*    const {user : {refreshToken, providerData},
+  } = await signInWithPopup(firebaseAuth, provider);
+    dispatch({
+      type : actionType.SET_USER,
+      user : providerData[0],
+    }) */
+  }
+
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
         {/*desktop & tablet*/}
         <div className="hidden md:flex w-full h-full items-center justify-between">
-          <div className="flex item-center gap-2">
-            <img src={Logo} className="w-8 object-cover" alt="Logo" />
+          <Link to={"/"} className="flex item-center gap-2">
+            <img src={Logo} className="w-8 object-cover cursor-pointer" alt="Logo"/>
             <p className="font-bold">City</p>
-          </div>
+          </Link>
 
-          <ul className="flex items-center gap-8">
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Menu</li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">About us</li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Service</li>
-          </ul>
+          <div className="flex items-center gap-8">
+            <ul className="flex items-center gap-8">
+              <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
+              <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Menu</li>
+              <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">About us</li>
+              <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Service</li>
+            </ul>
 
-          <div className="relative flex items-center justify-center">
-            <MdShoppingCart className="text-2xl ml-8 cursor-pointer"/>
-            <div className="absolute -top-1 -right-1 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
-              <p className="text-xs text-white font-semibold">2</p>
+            <div className="relative flex items-center justify-center">
+              <MdShoppingCart className="text-2xl ml-8 cursor-pointer"/>
+              <div className="absolute -top-1 -right-1 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
+                <p className="text-xs text-white font-semibold">2</p>
+              </div>
             </div>
-          </div>
 
+            <div className='relative'>
+            <motion.img
+              whileTap={{scale: 0.6}}
+              src={user ? user.photoURL : Avatar}
+              className="w-10 min-w-[40px] h-10 min-h[40px] drop-shadow-xl cursor-pointer rounded-full"
+              alt="profile"
+              onClick={login}/>
+            </div>
+
+          </div>
         </div>
 
         {/*mobile*/}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import { StateProvider } from './context/StateProvider';
+import reducer from './context/reducer';
+import { initialState } from './context/initialState';
 
 import App from './App';
 
 ReactDOM.render(
     <Router>
-        <App />
+        <StateProvider initialState={initialState} reduce={reducer}>
+            <App />
+        </StateProvider>
     </Router>,
     document.getElementById("root")
 );

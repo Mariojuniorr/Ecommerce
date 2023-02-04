6ddc948810663f4b3467dc06bb984a17356c827c
Mario Junior <mario-cruzeiro@hotmail.com>
2023-02-03 23:56:24 -0300

    HomeContainer Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
   const login = async() => {
     if(!user){
       const {
-        user: {refreshToken, providerData},
+        user: {providerData},
       } = await signInWithPopup(firebaseAuth, provider);
       dispatch({
         type : actionType.SET_USER,
@@ -49,7 +49,7 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
         {/*desktop & tablet*/}
         <div className="hidden md:flex w-full h-full items-center justify-between">
           <Link to={"/"} className="flex item-center gap-2">
@@ -100,8 +100,7 @@ const Header = () => {
                 }
                 <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100
                 transition-all duration-100 ease-in-out text-textcolor text-base'
-                onClick={logout}
-                >Logout <MdLogout/> </p>
+                onClick={logout}> Logout <MdLogout/> </p>
               </motion.div>
                 )
               }
